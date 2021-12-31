6ef07825530c4467328331d6714f690d1f7d3665
Jeonjunhowi <jjh63360@kau.kr>
2021-12-31 15:36:27 +0900

    ADD: Spring boot Server

---
+++ Home_garden_Server/src/main/java/com/kau/drimaes/home_garden/HomeGardenApplication.java
@@ -0,0 +1,13 @@
+package com.kau.drimaes.home_garden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeGardenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeGardenApplication.class, args);
+    }
+
+}
---
+++ Home_garden_Server/src/test/java/com/kau/drimaes/home_garden/HomeGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kau.drimaes.home_garden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeGardenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
