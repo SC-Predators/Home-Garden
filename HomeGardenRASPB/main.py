d2d2d60d246529e3f1bceaa9325810525a6f2b89
Jeonjunhowi <jjh63360@kau.kr>
2022-01-06 17:01:57 +0900

    ADD: 10초마다 웹캠 스크린샷 이미지 저장

--- HomeGardenRASPB/main.py
+++ HomeGardenRASPB/main.py
@@ -13,31 +13,29 @@ def createFolder(directory):
             os.makedirs(directory)
     except OSError:
         print('Error: Creating directory. ' + directory)
-createFolder("./" + homegarden_barcode)
 
 
-record = False
-#now는 사진이 저장될 이름 ex)2022-01-04+04-05-50.jpeg
-now = dt.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-#print(now)
+def capture(camid):
+    now = dt.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    cam = cv2.VideoCapture(camid)
+    if cam.isOpened() == False:
+        print('cant open the cam (%d)' % camid)
+        return None
 
-"""while True:
-    if (capture.get(cv2.CAP_PROP_POS_FRAMES) == capture.get(cv2.CAP_PROP_FRAME_COUNT)):
-        capture.open("/Image/Star.mp4")
+    ret, frame = cam.read()
+    if frame is None:
+        print('frame is not exist')
+        return None
 
-    ret, frame = capture.read()
-    cv2.imshow("VideoFrame", frame)
+    cv2.imwrite( "./" + homegarden_barcode +"/"+now+'.jpg', frame, params=[cv2.IMWRITE_JPEG_QUALITY, 60])
+    cam.release()
 
-    now = datetime.datetime.now().strftime("%d_%H-%M-%S")
-    key = cv2.waitKey(33)
+def mainloop():
+    while 1:
+        now = dt.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+        if dt.datetime.now().second%10 == 0:
+            capture(0)
 
-    if key == 27:
-        break
-    elif key == 26:
-        print("캡쳐")
-        cv2.imwrite("./"+ homegarden_barcode + str(now) + ".png", frame)
-
-
-capture.release()
-cv2.destroyAllWindows()
-"""
+if __name__ == '__main__':
+    createFolder("./" + homegarden_barcode)
+    mainloop()
