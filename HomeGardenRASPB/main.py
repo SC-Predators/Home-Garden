b42b7630174b0aa85ed733d4043a01f51269d187
Jeonjunhowi <jjh63360@kau.kr>
2022-01-07 14:47:29 +0900

    ADD: rds Update query

--- HomeGardenRASPB/main.py
+++ HomeGardenRASPB/main.py
@@ -11,7 +11,10 @@ import datetime as dt
 import os
 import base64
 import requests
+import random as rd
 import pymysql
+pymysql.install_as_MySQLdb()
+
 
 # pip install boto3
 # Module not found err: pip install opencv-python
@@ -19,7 +22,7 @@ import pymysql
 #--------------------------------rds연결 설정
 def connect_RDS(host, port, username, password, database):
     try:
-        conn = pymysql.connect(ck.host, user=username, passwd=password, db=database, port=port, use_unicode=True, charset= 'utf8')
+        conn = pymysql.connect(host=host, user=username, passwd=password, db=database, port=port, use_unicode=True, charset='utf8')
         cursor = conn.cursor()
 
     except:
@@ -49,8 +52,7 @@ def upload_img_to_s3(file_name): # f = 파일명
     s3.Bucket(ck.BUCKET_NAME).put_object(
         Key=file_name, Body=data, ContentType='image/jpg')
 
-def capture(camid):
-    now = dt.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+def capture(camid, now):
     cam = cv2.VideoCapture(camid)
     if cam.isOpened() == False:
         print('cant open the cam (%d)' % camid)
@@ -64,18 +66,34 @@ def capture(camid):
     file_name = homegarden_barcode +"/"+now+'.jpg'
     cv2.imwrite(file_name, frame, params=[cv2.IMWRITE_JPEG_QUALITY, 60])
     upload_img_to_s3(file_name)
+    cam.release()
+    return file_name
+
+def update_with_imgurl(conn, cursor, file_name, now):
+    updateQuery = """INSERT INTO Present_state (homegardenID, temperature, humidity, light, water_level, phStatus, img)
+                     VALUES ("{0}", {1}, {2}, {3}, {4}, {5}, "{6}")""".format(
+        homegarden_barcode,
+        rd.randint(10, 100),
+        rd.randint(10, 100),
+        rd.randint(10, 100),
+        rd.randint(10, 100),
+        rd.randint(10, 100),
+        "https://homegarden-images.s3.ap-northeast-2.amazonaws.com/homegarden1/" + now + ".jpg"
+    )
+    print("Query Updated: " + updateQuery)
+
+    cursor.execute(updateQuery)
+    conn.commit()
     os.remove(file_name)
 
-
-    cam.release()
-
 def mainloop():
+    conn, cursor = connect_RDS(ck.host, ck.port, ck.username, ck.password, ck.database)
     while 1:
         now = dt.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
         if dt.datetime.now().second%10 == 0:
-            capture(0)
+            file_name = capture(0, now)
+            update_with_imgurl(conn, cursor, file_name, now)
 
 if __name__ == '__main__':
     createFolder("./" + homegarden_barcode)
     mainloop()
-    conn, cursor = connect_RDS(ck.host, ck.port, ck.username, ck.password, ck.database)
