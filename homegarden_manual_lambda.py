b8ac9b95569b28a2f80c7c372567ae918a2c317f
Jeonjunhowi <jjh63360@kau.kr>
2022-01-14 14:17:07 +0900

    Update: lambda parsing logic

--- homegarden_manual_lambda.py
+++ homegarden_manual_lambda.py
@@ -16,12 +16,22 @@ print("############################")
 ################################
 
 def lambda_handler(event, context):
-    #print(event.type())
-    print(json.dumps(event, indent="\t"))
-    print("############################BODY")
     event_body = event['body']
-    print(event_body)
-    print("############################")
+    parsedBody = event_body.split(":")
+
+    print(parsedBody)
+    userSelected_clitentId = parsedBody[1].split("\"")
+    print("clientID: " + userSelected_clitentId[1])
+
+    userSelected_light: str = parsedBody[2].split(",")
+    print("light: " + userSelected_light[0])
+
+    userSelected_water = parsedBody[3][:-1].replace("\n", "")
+    print("water: " + str(userSelected_water))
+
+    payload = """{"state":{"reported":{"manual_light":""" + str(userSelected_light[0]) + """, "manual_water": """ + str(userSelected_water) + """}}}"""
+    print(payload)
+    original = """{"state":{"reported":{"manual_light":"on", "manual_water":"off"}}}"""
 
     _return_body_result = json.dumps(event_body)
     return {
