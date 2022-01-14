a421de1ad26956bebb634d2ee9c5d68c06e9e516
tpdms <tpdms0159@naver.com>
2022-01-14 10:32:55 +0900

    add serial

--- HomeGardenRASPB/HomeGarden_Arduino/HomeGarden_Arduino.ino
+++ HomeGardenRASPB/HomeGarden_Arduino/HomeGarden_Arduino.ino
@@ -1,11 +1,75 @@
-
 #define TdsSensorPin A1
-#define VREF 5.0 // analog reference voltage(Volt) of the ADC
-#define SCOUNT 30 // sum of sample point
+#define SCOUNT 30
+#define VREF 5.0 
+
+#include <ArduinoJson.h>
+
+
+
+//공중에서 약 1000
+//물속에서 280
+float _MAX = 1000;
+float _MIN = 280;
+
 int analogBuffer[SCOUNT]; // store the analog value in the array, read from ADC
 int analogBufferTemp[SCOUNT];
 int analogBufferIndex = 0,copyIndex = 0;
 float averageVoltage = 0,tdsValue = 0,temperature = 25;
+
+DynamicJsonDocument result_json(1024);
+
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  
+  float Soil_moisture = analogRead(A1);  
+  int light = analogRead(A0);
+  float humidity_rate = 100-((Soil_moisture-_MIN)/(_MAX-_MIN))*100;
+  
+  result_json["soil_humid"] = humidity_rate;
+  result_json["light"] = light;
+  
+  serializeJson(result_json, Serial);      
+  Serial.println("\n=================");
+  delay(1000);
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// analog reference voltage(Volt) of the ADC
+ // sum of sample point
 void setup()
 {
 Serial.begin(115200);
