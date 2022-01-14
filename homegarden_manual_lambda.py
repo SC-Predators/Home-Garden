17249ff2685544fa4dba1b932f15bd179fa5250c
Jeonjunhowi <jjh63360@kau.kr>
2022-01-14 11:20:36 +0900

    Update: python lambda parser

--- homegarden_manual_lambda.py
+++ homegarden_manual_lambda.py
@@ -1,14 +1,35 @@
-context = 0
 import json
 
+#################################개발단지
+context = 0
+with open('./sampleEvent.json', 'r') as f:
+    event = json.load(f)
+
+"""
+print(json.dumps(event, indent="\t") )
+print("############################BODY")
+event_body = event['body']
+print(event_body)
+print("############################")
+"""
+
+################################
+
 def lambda_handler(event, context):
-    tempStr = json.dumps(event)
+    #print(event.type())
+    print(json.dumps(event, indent="\t"))
+    print("############################BODY")
+    event_body = event['body']
+    print(event_body)
+    print("############################")
+
+    _return_body_result = json.dumps(event_body)
     return {
         'statusCode': 200,
-        'body': tempStr,
+        'body': _return_body_result,
         "headers": {
             "content-type": "application/json"
         }
     }
 
-#lambda_handler(event, context)
+print(lambda_handler(event, context))
