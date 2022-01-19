9df601309fdaf444628c379ef07e83c94e72e0d7
tpdms <tpdms0159@naver.com>
2022-01-19 14:29:11 +0900

    add arduino final

--- HomeGardenRASPB/HomeGarden_Arduino/HomeGarden_Arduino.ino
+++ HomeGardenRASPB/HomeGarden_Arduino/HomeGarden_Arduino.ino
@@ -3,7 +3,7 @@
 #define VREF 5.0 
 #define TRIG 8
 #define ECHO 9
-
+#define Relay 12
 #include <ArduinoJson.h>
 
 
@@ -12,16 +12,8 @@
 //물속에서 280
 float _MAX = 1000;
 float _MIN = 280;
-
-int analogBuffer[SCOUNT]; // store the analog value in the array, read from ADC
-int analogBufferTemp[SCOUNT];
-int analogBufferIndex = 0,copyIndex = 0;
-float averageVoltage = 0,tdsValue = 0,temperature = 25;
-
-int Relay = A5;
-
-
-
+String relay_state;
+  
 
 DynamicJsonDocument result_json(1024);
 
@@ -31,43 +23,12 @@ void setup() {
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
   pinMode(Relay, OUTPUT);
-  
-  
 }
-
-
-
-
-// analog reference voltage(Volt) of the ADC
- // sum of sample point
-int getMedianNum(int bArray[], int iFilterLen)
-{
-  int bTab[iFilterLen];
-  for (byte i = 0; i<iFilterLen; i++)
-    bTab[i] = bArray[i];
-    int i, j, bTemp;
-    for (j = 0; j < iFilterLen - 1; j++)
-    {
-      for (i = 0; i < iFilterLen - j - 1; i++)
-      {
-      if (bTab[i] > bTab[i + 1])
-        {
-        bTemp = bTab[i];
-        bTab[i] = bTab[i + 1];
-        bTab[i + 1] = bTemp;
-      }
-    }
-  }
-  if ((iFilterLen & 1) > 0)
-    bTemp = bTab[(iFilterLen - 1) / 2];
-  else
-    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
-  return bTemp;
-}
-
 
  
 void loop() {
+  relay_state = digitalRead(Relay);
+  
   //초음파 센서
   long duration, distance;
 
@@ -79,7 +40,6 @@ void loop() {
   
   duration = pulseIn (ECHO, HIGH);
   distance = duration * 17/1000;
-  Serial.println(distance); 
 
 
   
@@ -96,17 +56,24 @@ void loop() {
   result_json["depth"] = distance;
 
   
+
+
+  if (soil > 4){
+    // 물펌프 동작
+    Serial.print("waterPumpOn");
+    digitalWrite(Relay, HIGH);
+  }
+  else {
+    Serial.print("waterPumpOff");
+    digitalWrite(Relay, LOW);
+  }
   
+  result_json["pumpState"] = digitalRead(Relay);
+
   serializeJson(result_json, Serial);      
   Serial.println("\n=================");
   delay(1000);
 
-  if (soil > 4){
-    // 물펌프 동작
-    Serial.print("waterPump");
-    digitalWrite(Relay, HIGH);
-    delay(1000);
-    digitalWrite(Relay, LOW);
-  }
- 
+  
+  
 }
